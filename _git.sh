81d6ad67e8e6e6fb779c278ebf6c09b55f90f788
lauxinyi <lauxinyi@gmail.com>
2021-06-30 10:05:21 +0800

    master

---
+++ _git.sh
@@ -0,0 +1,7 @@
+git config user.name "lauxinyi"
+git config user.email "lauxinyi@gmail.com"
+git pull
+git status
+git add -A
+git commit -m "master"
+git push 
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lsamu/ago
+
+go 1.16
