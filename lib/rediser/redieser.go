0e8464becd4eb5fc3bc94d6db9639654f2683122
lauxinyi <lauxinyi@gmail.com>
2021-07-05 10:34:19 +0800

    master

---
+++ lib/rediser/redieser.go
@@ -0,0 +1,32 @@
+package rediser
+
+
+import (
+    "github.com/gomodule/redigo/redis"
+    "github.com/lsamu/ago/lib/cache"
+    "time"
+)
+
+var redisConn *cache.Rediser
+
+// 连接redis
+func InitRedis(address string,password string,db int) {
+    conn, err := redis.Dial("tcp", address,
+        redis.DialReadTimeout(10*time.Second),
+        redis.DialWriteTimeout(10*time.Second),
+        redis.DialConnectTimeout(10*time.Second),
+        redis.DialDatabase(db),
+        redis.DialPassword(password),
+    )
+    if err != nil {
+        panic(err)
+    }
+    redisConn = cache.NewSubCache(conn, conn)
+    return
+}
+
+// 连接redis
+func RedisConn() *cache.Rediser {
+    return redisConn
+}
+
