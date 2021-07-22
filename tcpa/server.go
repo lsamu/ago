831daca9fc14e327148fca3a024282fea6739397
lauxinyi <lauxinyi@gmail.com>
2021-07-22 14:51:05 +0800

    master

--- tcpa/server.go
+++ tcpa/server.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
-	
+import (
+    "fmt"
+    "net"
+)
+
+type Server struct {
+
+}
+
+func (s *Server) Start() {
+    fmt.Println("Starting the server ...")
+    listener, err := net.Listen("tcp", "localhost:50000")
+    if err != nil {
+        fmt.Println("Error listening", err.Error())
+        return //终止程序
+    }
+    // 监听并接受来自客户端的连接
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            fmt.Println("Error accepting", err.Error())
+            return // 终止程序
+        }
+        go s.doServerStuff(conn)
+    }
+}
+func (s *Server) doServerStuff(conn net.Conn) {
+    for {
+        buf := make([]byte, 512)
+        len, err := conn.Read(buf)
+        if err != nil {
+            fmt.Println("Error reading", err.Error())
+            return //终止程序
+        }
+        fmt.Printf("Received data: %v\n", string(buf[:len]))
+    }
 }
