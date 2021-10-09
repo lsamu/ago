db59e1396a1bb799e40c5d1fb7872ab7231f4358
lauxinyi <lauxinyi@gmail.com>
2021-10-09 13:05:10 +0800

    master

---
+++ store/minio/minio.go
@@ -0,0 +1,19 @@
+package minio
+
+
+func InitMinioOrm() {
+
+}
+
+func GetMinioOrm() {
+
+}
+
+func NewMiniOrm() *MinioOrm {
+    orm := &MinioOrm{}
+    return orm
+}
+
+type MinioOrm struct {
+
+}
