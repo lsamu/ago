5e432befe1f11fb0bd0cb354fb74ef32887138d1
lauxinyi <lauxinyi@gmail.com>
2021-10-28 14:36:36 +0800

    master

--- rest/use/tls.go
+++ rest/use/tls.go
@@ -1,15 +1,21 @@
 package use
 
 import (
+    "fmt"
     "github.com/gin-gonic/gin"
     "github.com/unrolled/secure"
 )
 
-func TlsHandler() gin.HandlerFunc {
+//TlsHandler tls传输  domain:xxx.com     使用 router.RunTLS(":8080", "ssl.pem", "ssl.key")
+func TlsHandler(domain string) gin.HandlerFunc {
     return func(c *gin.Context) {
+        if domain == "" {
+            fmt.Println("abort tls")
+            c.Abort()
+        }
         secureMiddleware := secure.New(secure.Options{
             SSLRedirect: true,
-            SSLHost:     "xxx.com", // config.SslConfig.Domain,
+            SSLHost:     domain, // config.SslConfig.Domain,
         })
         err := secureMiddleware.Process(c.Writer, c.Request)
         if err != nil {
