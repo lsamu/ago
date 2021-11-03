8fa3967ad670736735ff48d89347f9e89eebe6dc
lauxinyi <lauxinyi@gmail.com>
2021-11-03 15:35:01 +0800

    feat: 修改脚本提交。

--- _git.sh
+++ _git.sh
@@ -1,7 +1,8 @@
+read -p "请输入本次提交的描述：" remark
 git config user.name "lauxinyi"
 git config user.email "lauxinyi@gmail.com"
 git pull
 git status
 git add -A
-git commit -m "master"
+git commit -m "$remark"
 git push 
